f3e6f77c27cd052ab6687ee43d2b601afa694b3a
Griffin T <griffin_git@thommo.io>
2025-05-20 05:02:18 +0800

    start of a cli with error handling and logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, World!");
+use std::{fs, io::Read, path::PathBuf};
+
+use clap::Parser;
+use color_eyre::Result;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{EnvFilter, prelude::*};
+
+/// har
+///
+/// Parse The Simpsons Hit & Run save files.
+#[derive(Debug, Parser)]
+#[command(about, version)]
+struct Args {
+    /// Path to a save file
+    save_path: PathBuf,
+}
+
+fn setup() -> Result<Args> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    color_eyre::install()?;
+    Ok(Args::parse())
+}
+
+fn main() -> Result<()> {
+    let args = setup()?;
+    dbg!(&args);
+
+    let file = fs::File::open(args.save_path)?;
+    let mut reader = libhar::io::ChunkReader::new(file);
+
+    let mut read_buffer = [0u8; 512];
+    let mut total_len = 0;
+    loop {
+        let len = reader.read(&mut read_buffer)?;
+        if len == 0 {
+            break;
+        }
+
+        info!("read {} bytes", len);
+        total_len += len;
+    }
+
+    info!("total bytes: {}", total_len);
+
+    Ok(())
 }
